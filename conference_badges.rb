218c5f1b446a7c0153b4bbc4d449f4a6a67e3828
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-13 09:35:33 +0300

    completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+    arr.map do |name|
+        "Hello, my name is #{name}."
+    end
+
+end
+
+def assign_rooms arr
+    arr.map {|person| "Hello, #{person}! You'll be assigned to room #{(arr.find_index person) + 1}!"}
+end
+
+def printer arr
+
+    rooms = assign_rooms(arr)
+    rooms.each {|room| puts room}
+
+    badge_create = batch_badge_creator(arr)
+    badge_create.each {|badge| puts badge}
+
+end
